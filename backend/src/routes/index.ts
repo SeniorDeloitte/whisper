c6e2e311e88e6e2fce154e53612887927c255947
mhpm <michelleeepm@gmail.com>
2026-03-06 17:55:57 -0600

    feat: Create index file to centralize the export of all route modules.

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,7 @@
+import authRoutes from "./authRoutes";
+import chatRoutes from "./chatRoutes";
+import messageRoutes from "./messageRoutes";
+import userRoutes from "./userRoutes";
+import webhookRoutes from "./webhookRoutes";
+
+export { authRoutes, chatRoutes, messageRoutes, userRoutes, webhookRoutes };
